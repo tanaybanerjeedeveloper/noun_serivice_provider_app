e1da651cb349263698656146b1c51f9f49c3bcee
tanaybanerjeedeveloper <banerjeetanay333@gmail.com>
2022-06-03 13:37:30 +0530

    fix: fixed the login issue

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import '../utilities/constants.dart';
 import '../widgets/3rd_party_login_btn.dart';
@@ -7,6 +8,7 @@ import '../widgets/button.dart';
 import 'home_screen.dart';
 import '../widgets/bottom_navigation_bar.dart';
 import 'otp_screen.dart';
+import '../utilities/config.dart';
 
 class LoginScreen extends StatefulWidget {
   //LoginScreen({Key? key}) : super(key: key);
@@ -19,144 +21,190 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   var _isElevated = true;
   var _checkBoxValue = false;
+  String? _phoneNumber;
+  final GlobalKey<FormState> key = GlobalKey<FormState>();
+
+  Future<void> _login(String phone) async {
+    print('phone: $phone');
+    try {
+      var response =
+          await http.post(Uri.parse('$baseURL/station/service-provider-login'),
+              body: json.encode(
+                {"phone": phone},
+              ),
+              headers: {'Content-Type': 'application/json'});
+      if (response.statusCode == 201) {
+        print('response: ${response.body}');
+        print('account created successfully');
+      } else {
+        print('failed');
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: mediaQuery.size.height * 1,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/background.png'),
-              fit: BoxFit.cover,
+      body: Form(
+        key: key,
+        child: SingleChildScrollView(
+          child: Container(
+            height: mediaQuery.size.height * 1,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/background.png'),
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/logo.png',
-                  height: 120,
-                  fit: BoxFit.cover,
-                ),
-                const SizedBox(
-                  height: 45.0,
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(
-                      horizontal: 30.0, vertical: 0.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25.0),
-                    boxShadow: [
-                      const BoxShadow(
-                          color: Color(0xff00ffba),
-                          offset: Offset(2, 2),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/logo.png',
+                    height: 120,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(
+                    height: 45.0,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 30.0, vertical: 0.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25.0),
+                      boxShadow: [
+                        const BoxShadow(
+                            color: Color(0xff00ffba),
+                            offset: Offset(2, 2),
+                            blurRadius: 10,
+                            spreadRadius: 0.2),
+                        const BoxShadow(
+                          color: Colors.black,
+                          offset: Offset(-4, -4),
                           blurRadius: 10,
-                          spreadRadius: 0.2),
-                      const BoxShadow(
-                        color: Colors.black,
-                        offset: Offset(-4, -4),
-                        blurRadius: 10,
+                        )
+                      ],
+                    ),
+                    child: TextFormField(
+                      // onChanged: (value) {
+                      //   _phoneNumber = value;
+                      // },
+                      validator: (value) {
+                        print('value: $value');
+                        if (value!.isEmpty) {
+                          return 'Enter number';
+                        } else {
+                          _phoneNumber = value;
+                          print('no: $_phoneNumber');
+                        }
+                      },
+                      decoration:
+                          kTextFieldDecoration.copyWith(hintText: 'Phone'),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 55.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SmallButton('assets/images/fb.png'),
+                      const SizedBox(
+                        width: 20.0,
+                      ),
+                      SmallButton('assets/images/gmail.png'),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 55.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        child: Theme(
+                          child: Checkbox(
+                            value: _checkBoxValue,
+                            shape: CircleBorder(),
+                            activeColor: Color(0xff00ffba),
+                            onChanged: (value) {
+                              setState(() {
+                                _checkBoxValue = value!;
+                              });
+                            },
+                          ),
+                          data: ThemeData(
+                              unselectedWidgetColor: Color(0xff1f1f1f)),
+                        ),
+                        height: 20,
+                        width: 20,
+                        decoration: BoxDecoration(
+                            color: Color(0xff1f1f1f),
+                            borderRadius: BorderRadius.circular(50),
+                            boxShadow: [
+                              const BoxShadow(
+                                color: Color(0xff00ffba),
+                                offset: Offset(2, 2),
+                                blurRadius: 10,
+                                spreadRadius: 0.5,
+                              ),
+                              const BoxShadow(
+                                color: Colors.black,
+                                offset: Offset(-4, -4),
+                                blurRadius: 10,
+                              )
+                            ]),
+                      ),
+                      SizedBox(width: 10.0),
+                      Text(
+                        'Terms and Condition',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w800, fontSize: 14.0),
                       )
                     ],
                   ),
-                  child: TextField(
-                    onChanged: (value) {},
-                    decoration:
-                        kTextFieldDecoration.copyWith(hintText: 'Phone/Email'),
+                  const SizedBox(
+                    height: 75.0,
                   ),
-                ),
-                const SizedBox(
-                  height: 55.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SmallButton('assets/images/fb.png'),
-                    const SizedBox(
-                      width: 20.0,
-                    ),
-                    SmallButton('assets/images/gmail.png'),
-                  ],
-                ),
-                const SizedBox(
-                  height: 55.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      child: Theme(
-                        child: Checkbox(
-                          value: _checkBoxValue,
-                          shape: CircleBorder(),
-                          activeColor: Color(0xff00ffba),
-                          onChanged: (value) {
-                            setState(() {
-                              _checkBoxValue = value!;
-                            });
-                          },
+                  Button(
+                    'LOGIN',
+                    () {
+                      print('from here: $_phoneNumber');
+                      if (key.currentState!.validate()) {
+                        _login(_phoneNumber!);
+                      }
+                    },
+                  ),
+                  // ElevatedButton(
+                  //     onPressed: () {
+                  //       print('from here: $_phoneNumber');
+                  //       if (key.currentState!.validate()) {
+                  //         _login(_phoneNumber!);
+                  //       }
+                  //     },
+                  //     child: Text('data')),
+                  const SizedBox(
+                    height: 35.0,
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      text: 'Dont Have an account? ',
+                      //style: DefaultTextStyle.of(context).style,
+                      children: const <TextSpan>[
+                        TextSpan(
+                          text: 'Login',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.red),
                         ),
-                        data:
-                            ThemeData(unselectedWidgetColor: Color(0xff1f1f1f)),
-                      ),
-                      height: 20,
-                      width: 20,
-                      decoration: BoxDecoration(
-                          color: Color(0xff1f1f1f),
-                          borderRadius: BorderRadius.circular(50),
-                          boxShadow: [
-                            const BoxShadow(
-                              color: Color(0xff00ffba),
-                              offset: Offset(2, 2),
-                              blurRadius: 10,
-                              spreadRadius: 0.5,
-                            ),
-                            const BoxShadow(
-                              color: Colors.black,
-                              offset: Offset(-4, -4),
-                              blurRadius: 10,
-                            )
-                          ]),
+                      ],
                     ),
-                    SizedBox(width: 10.0),
-                    Text(
-                      'Terms and Condition',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w800, fontSize: 14.0),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 75.0,
-                ),
-                Button('LOGIN', () {
-                  // Navigator.pushNamed(context, BottomNavigation.id);
-                  // Timer(const Duration(milliseconds: 500),
-                  //     () => Navigator.pushNamed(context, BottomNavigation.id));
-                  Timer(const Duration(milliseconds: 500),
-                      () => Navigator.pushNamed(context, OtpScreen.routeName));
-                }),
-                const SizedBox(
-                  height: 35.0,
-                ),
-                RichText(
-                  text: TextSpan(
-                    text: 'Dont Have an account? ',
-                    //style: DefaultTextStyle.of(context).style,
-                    children: const <TextSpan>[
-                      TextSpan(
-                        text: 'Login',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.red),
-                      ),
-                    ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
---
+++ lib/utilities/config.dart
@@ -0,0 +1,1 @@
+const baseURL = 'http://192.168.0.125:2060/api/v1';
